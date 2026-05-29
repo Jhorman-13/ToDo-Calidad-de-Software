23d172c3315a4073f342f930e37d94b9333a082f
Jhorman Cadavid <jhorman.cadavid184@pascualbravo.edu.co>
2026-05-29 13:47:06 -0500

    test: desactivar allowCypressEnv y chromeWebSecurity para Electron

--- todo-frontend/cypress.config.js
+++ todo-frontend/cypress.config.js
@@ -2,7 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3001', // <-- Verifica que este puerto sea el correcto de tu contenedor frontend
+    baseUrl: 'http://localhost:3001', // <-- Asegúrate de que sea el puerto de tu login front
+    allowCypressEnv: false,           // <-- Apaga la advertencia de seguridad que sale en el log
+    chromeWebSecurity: false,         // <-- Evita que Electron bloquee peticiones entre el front y el back
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
