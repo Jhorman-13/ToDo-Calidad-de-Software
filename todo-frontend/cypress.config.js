a0f5b9162302eaef217475227d93f40ae30eccdd
Jhorman Cadavid <jhorman.cadavid184@pascualbravo.edu.co>
2026-05-29 14:48:11 -0500

    Creacion del video para la prueba end to end

--- todo-frontend/cypress.config.js
+++ todo-frontend/cypress.config.js
@@ -2,8 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-  video: true,
-      trashAssetsBeforeRuns: true,
+    video: true,
+    trashAssetsBeforeRuns: true,
     baseUrl: 'http://localhost:3001', // <-- Asegúrate de que sea el puerto de tu login front
     allowCypressEnv: false,           // <-- Apaga la advertencia de seguridad que sale en el log
     chromeWebSecurity: false,         // <-- Evita que Electron bloquee peticiones entre el front y el back
